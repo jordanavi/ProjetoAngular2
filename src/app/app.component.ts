58cab3255648d46d5b9b2713fa02797393a49151
jordanavi <jordanavilela01@gmail.com>
2019-04-03 21:55:33 -0300

    Projeto feito na aula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  pessoas: string[] = ['Joao', 'aaa', 'Ze'];
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HomeComponent } from './home/home.component';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+import { RoutingModule } from './app.router'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
